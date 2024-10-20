877e877b05662df6d8c164c604362b7101af740d
Andreas Johansson <glanshammar70@gmail.com>
2024-10-20 18:27:27 +0200

    Added base stylesheet to script

--- scripts/header-footer.js
+++ scripts/header-footer.js
@@ -1,11 +1,12 @@
 $(function() {
     const headerElement = $('#header');
     const footerElement = $('#footer');
+    const pageHead = $('head');
 
     const headerContent = `
     <h1>JavaScript Apps Project</h1>
         <nav>
-            <a href="../index.html">Home</a>
+            <a href="../pages/index.html">Home</a>
             <a href="../pages/bishbosh.html">Bish Bosh</a>
             <a href="../pages/todo.html">ToDo</a>
             <a href="../pages/starwars.html">Star Wars Index</a>
@@ -15,8 +16,9 @@ $(function() {
 
     const footerContent = `
         <p>©2024 Andreas Johansson. All rights reserved.</p>
-    `
+    `;
 
     headerElement.html(headerContent).append('<link rel="stylesheet" type="text/css" href="../styles/header.css">');
     footerElement.html(footerContent).append('<link rel="stylesheet" type="text/css" href="../styles/footer.css">');
+    pageHead.append('<link rel="stylesheet" type="text/css" href="../styles/base.css">');
 })
